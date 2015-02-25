3a38108df4d87a4e4ec32ad81c31f2b116cdba89
David Prince <davidprince@Pelican.local>
2015-02-25 00:12:54 -0500

    Added in getSelectedRowNum method.

--- ObjectLabSoftware/src/javaapplication3/UtilController.java
+++ ObjectLabSoftware/src/javaapplication3/UtilController.java
@@ -3,11 +3,30 @@
  */
 package javaapplication3;
 
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author nick
  */
 public class UtilController 
 {
-        
+     /**
+      * gets row number of matching column in dm
+      * @param dm the table model
+      * @param column
+      * @param selectedRow current row selected
+      * @return 
+      */
+    public static int getSelectedRowNum(DefaultTableModel dm, int column, int selectedRow)
+    {
+        for (int i = 0; i < dm.getRowCount(); i++)
+        {
+            if (dm.getValueAt(i, column).equals(dm.getValueAt(selectedRow, column)))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
 }
