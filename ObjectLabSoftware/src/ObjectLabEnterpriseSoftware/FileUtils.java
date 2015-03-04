88c776b3c5f655c84eb2cde1684255ab5c3a7274
nlicci1 <nlicci1@students.towson.edu>
2015-03-04 00:57:31 -0500

    Finished refactoring PendingJobsView.java. Removed RejectDescription.java, and ApprovePage.java. Replaced the view from those classes and made them simple JOption pane input fields (with help of Sean from UI) that had the input data handed off to the UtilController method calls. Added new column into PendingJobsView (for first and last name). Fixed bug with the PDF help Admin not opening. Fixed some bugs related to not closing DB connections by remvoving the static DB connection variable in PendingJobsView. Fixed file locations not being updated properly when a file was approved.

---
+++ ObjectLabSoftware/src/ObjectLabEnterpriseSoftware/FileUtils.java
@@ -0,0 +1,246 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ObjectLabEnterpriseSoftware;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import org.apache.commons.io.FileExistsException;
+import org.apache.poi.ss.usermodel.Workbook;
+
+/**
+ *
+ * @author Miguel
+ */
+public class FileUtils {
+    
+    private static final InstanceCall cloudStorageOperations = new InstanceCall();
+    private final String submission;
+    private final String drive;
+    private String zcorpToPrint;
+    private final String zcorpPrinted;
+    private String zcorpToBuildDir;
+    private final String zcorpToBuild;
+    private final String solidscapeToPrint;
+    private final String solidscapePrinted;
+    private final String solidscapeToBuild;
+    private final String objetToPrint;
+    private final String objetPrinted;
+    private final String objetToBuild;
+    private final String PDFAdmin;
+    private final String PDFStudent;
+    private String submitted;
+    private final String rejected;
+    private final String excelFilePath;
+    private final String INPUT_FOLDER;
+    private final String ZIPPED_FOLDER;
+
+
+    //Sets default locations that will be shared by all installations
+    public FileUtils() {
+        //Use the directory provided on piazza and change the file paths below to test
+        drive = "C:\\Sync";
+        rejected = drive + "\\ObjectLabPrinters\\Rejected\\";
+        submission = drive + "\\ObjectLabPrinters\\Submissions\\";
+        PDFAdmin = "\\TomSoft Help Admin.pdf";
+        PDFStudent = "\\TomSoft Help Student.pdf";
+        zcorpToPrint = drive + "\\ObjectLabPrinters\\Zcorp\\ToPrint\\";
+        zcorpPrinted = drive + "\\ObjectLabPrinters\\Zcorp\\Printed\\";
+        zcorpToBuild = drive + "\\ObjectLabPrinters\\Zcorp\\Build Files\\";
+        solidscapeToPrint = drive + "\\ObjectLabPrinters\\Solidscape\\ToPrint\\";
+        solidscapePrinted = drive + "\\ObjectLabPrinters\\Solidscape\\Printed\\";
+        solidscapeToBuild = drive + "\\ObjectLabPrinters\\Solidscape\\Build Files\\";
+        objetToPrint = drive + "\\ObjectLabPrinters\\Objet\\ToPrint\\";
+        objetPrinted = drive + "\\ObjectLabPrinters\\Objet\\Printed\\";
+        objetToBuild = drive + "\\ObjectLabPrinters\\Objet\\Build Files\\";
+        excelFilePath = drive + "\\Export\\";
+        ZIPPED_FOLDER = drive + "\\ObjectLabPrinters\\";
+        INPUT_FOLDER = drive + "\\ObjectLabPrinters\\";
+    }
+    
+    public boolean deleteFile(String path){
+        File newDir = new File(path);
+        if(newDir.exists()){
+            newDir.delete();
+        }
+        
+        if(newDir.exists()) return true;
+        
+        return false;
+    }
+    
+    public boolean doesFileExist(String path){
+    
+        File newDir = new File(path);
+        if(newDir.exists()){
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean rejectFile(String FileName){
+        try {
+            org.apache.commons.io.FileUtils.moveFileToDirectory(new File(submission + FileName), new File(rejected), true);
+        } catch (IOException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean approveFile(String FileName, String printer){
+    
+        File newDir = new File(drive + "\\ObjectLabPrinters\\" + printer + "\\ToPrint");
+            try {
+                org.apache.commons.io.FileUtils.moveFileToDirectory(new File(cloudStorageOperations.getSubmission() + "\\" + FileName), newDir, true);
+            } catch (FileExistsException e) {
+                org.apache.commons.io.FileUtils.deleteQuietly(new File(newDir.getAbsoluteFile() + FileName));
+                newDir = new File(cloudStorageOperations.getDrive() + "\\ObjectLabPrinters\\" + printer + "\\ToPrint");
+                try {
+                    org.apache.commons.io.FileUtils.moveFileToDirectory(new File(cloudStorageOperations.getSubmission() + "\\" + FileName), newDir, true);
+                } catch (IOException ex) {
+                    //Logger.getLogger(ApprovePage.class.getName()).log(Level.SEVERE, null, ex);
+                    return false;
+                }
+            } catch (IOException ex) {
+                //Logger.getLogger(ApprovePage.class.getName()).log(Level.SEVERE, null, ex);
+                return false;
+            }
+    
+        return true;
+    }
+    
+    public boolean saveReport(String name, Workbook wb){
+        
+        try {
+            FileOutputStream out = new FileOutputStream(excelFilePath + name + ".xls");
+            wb.write(out);
+            out.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        } catch (IOException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public boolean submitFile(String fileLocation){
+        
+        try {
+            org.apache.commons.io.FileUtils.copyFileToDirectory(new File(fileLocation), new File(submission));
+        } catch (IOException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public String browseForFile(){
+    
+        JFileChooser fileopen = new JFileChooser();  //in brackets, add Syncthing directory or new Drive's address for default location
+        //Limits selected files to the following types. TODO fix list
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Model Files", ".stl", ".obj", ".zpr");
+        fileopen.setFileFilter(filter);
+
+        fileopen.setAcceptAllFileFilterUsed(false);
+        int ret = fileopen.showDialog(null, "Open file");
+
+        if (ret == JFileChooser.APPROVE_OPTION) {
+            File file = fileopen.getSelectedFile();
+            //Inputs the file location into the textbox "fileName"
+            return file.toString().replaceAll("'", "");
+        }
+        return null;
+        
+    }
+    
+    public boolean zipFilesTo(String zipFileName){
+        String zipTo =  ZIPPED_FOLDER + zipFileName + ".zip";
+        try {
+            zip(INPUT_FOLDER, zipTo);
+        } catch (IOException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+           return true;
+    }
+    public static void zip(String inputFolder,String targetZippedFolder)  throws IOException {
+
+        FileOutputStream fileOutputStream = null;
+
+        fileOutputStream = new FileOutputStream(targetZippedFolder);
+        ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
+        File inputFile = new File(inputFolder);
+        if (inputFile.isFile())
+            zipFile(inputFile,"",zipOutputStream);
+        else if (inputFile.isDirectory())
+            zipFolder(zipOutputStream,inputFile,"");
+
+
+        zipOutputStream.close();
+    }
+
+    public static void zipFolder(ZipOutputStream zipOutputStream,File inputFolder, String parentName)  throws IOException {
+ 
+        String myname = parentName +inputFolder.getName()+"\\";
+
+        ZipEntry folderZipEntry = new ZipEntry(myname);
+        zipOutputStream.putNextEntry(folderZipEntry);
+
+        
+        File[] contents = inputFolder.listFiles();
+        for (File f : contents){
+            if (f.isFile())
+                zipFile(f,myname,zipOutputStream);
+
+            else if(f.isDirectory())
+                zipFolder(zipOutputStream,f, myname);
+        }
+        zipOutputStream.closeEntry();
+    }
+
+    public static void zipFile(File inputFile,String parentName,ZipOutputStream zipOutputStream) throws IOException{
+ 
+        // A ZipEntry represents a file entry in the zip archive
+        // We name the ZipEntry after the original file's name
+
+        ZipEntry zipEntry = new ZipEntry(parentName+inputFile.getName());
+
+        zipOutputStream.putNextEntry(zipEntry);
+
+        FileInputStream fileInputStream = new FileInputStream(inputFile);
+
+        byte[] buf = new byte[1024];
+
+        int bytesRead;
+
+        // Read the input file by chucks of 1024 bytes
+        // and write the read bytes to the zip stream
+        while ((bytesRead = fileInputStream.read(buf)) > 0) {
+            
+            zipOutputStream.write(buf, 0, bytesRead);
+        }
+        // close ZipEntry to store the stream to the file
+        zipOutputStream.closeEntry();
+ 
+       // System.out.println("Regular file :" + parentName+inputFile.getName() +" is zipped to archive :"+ZIPPED_FOLDER);
+    }
+
+       
+    
+}
---
+++ ObjectLabSoftware/src/ObjectLabEnterpriseSoftware/UtilController.java
@@ -0,0 +1,242 @@
+/*
+ *
+ */
+package ObjectLabEnterpriseSoftware;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+import static org.apache.commons.io.FileUtils.directoryContains;
+/**
+ *
+ * @author nick
+ */
+public class UtilController 
+{
+    private static final boolean SUCCESS = true;
+    private static final boolean FAILURE = false;
+    
+    public static boolean rejectStudentSubmission(String file, String fName, String lName, String dateOfSubmission, String reasonForRejection)
+    {
+        SQLMethods dbconn = new SQLMethods();
+        ResultSet results = dbconn.searchID("pendingjobs", fName, lName, file, dateOfSubmission);
+        InstanceCall cloudStorageOperations = new InstanceCall();
+        
+        try 
+        {
+            if (results.next()) 
+            {
+                /*
+                   This checks to see if the file exists in the rejected location if it does the rejection will fail
+                    and the file that is in the reject location will be deleted. *hot fix for bug*
+                   If the file does not exist it is moved into the rejected location and the entry in the DB for that
+                    submission will be deleted. An email is sent to the user that the file was not accepted.
+                    -Nick 
+                */
+                File locationOfRejectedFiles = new File(cloudStorageOperations.getRejected());
+                
+                if(!directoryContains(locationOfRejectedFiles, new File(cloudStorageOperations.getRejected() + file)))
+                {
+                    org.apache.commons.io.FileUtils.moveFileToDirectory(new File(cloudStorageOperations.getSubmission() + file), locationOfRejectedFiles, true);
+                    
+                    SendEmail rejectionEmail = new SendEmail(fName, lName, reasonForRejection, file, results.getString("idJobs"));
+                    rejectionEmail.Send();
+                    
+                    dbconn.delete("pendingjobs", results.getString("idJobs"));
+                    dbconn.closeDBConnection();
+                    
+                    return SUCCESS;
+                }
+                else
+                {
+                    System.out.println("File deleted");
+                    org.apache.commons.io.FileUtils.forceDelete(new File(cloudStorageOperations.getRejected() + file));
+                }
+                
+            }
+            
+            return FAILURE;
+        } 
+        catch (SQLException | IOException ex) 
+        {
+            System.out.println("Program crashed in reject subm\n" + ex);
+            return FAILURE;
+        }
+    }
+    
+    public static void approveStudentSubmission(String fileName, String firstName, String lastName, String printer, String dateStarted, double volume)
+    {
+        /* Make the connection to our DB and query for the PK of pendingjobs which is a combination of
+           all the fields input in the searchID method call
+        */
+        SQLMethods dbconn = new SQLMethods();
+        ResultSet result = dbconn.searchID("pendingjobs", firstName, lastName, fileName, dateStarted);
+        InstanceCall cloudStorageOperations = new InstanceCall();
+        
+        String ID;
+
+        try 
+        { 
+            /* If the row exist, then query for the PK and then use that to update the pendingjobs table fileLocation. -Nick */
+            if (result.next())
+            {
+                ID = result.getString("idJobs");
+                String updatedDirectoryLocation = cloudStorageOperations.getDrive() + "\\ObjectLabPrinters\\" + printer + "\\ToPrint";
+                String updatedFileLocation = updatedDirectoryLocation + "\\" + fileName;
+                String currentFileLocation = cloudStorageOperations.getSubmission() + "\\" + fileName;
+                
+                /* This moves the file from the submissions folder to the toPrint folder in folder specified by 
+                 *  the printer variable -Nick
+                 */
+                org.apache.commons.io.FileUtils.moveFileToDirectory(new File(currentFileLocation), new File(updatedDirectoryLocation), true);
+                
+                /* In order to properly update the file location we need to gurantee there are '\\' seperating
+                 *  the dir names.
+                 *  If ther are no double backslashes than the character will not be escaped properly, and
+                 *  the DB will not contain the correct file location.
+                 *   - Nick
+                 */           
+                dbconn.updatePendingJobVolume(ID, volume);
+                dbconn.updatePendingJobFLocation(ID, updatedFileLocation.replace("\\", "\\\\"));
+                dbconn.approve(ID);
+                dbconn.closeDBConnection();
+            }
+        } 
+        catch (SQLException | IOException ex)
+        {
+            Logger.getLogger(UtilController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /* This function has the params that make up the primary key in pending jobs.
+       It returns the filePath found in the DB where all the parameters specify the file
+       that is associated for that users submission.
+     */
+    public static File getFilePath(String firstName, String lastName, String fileName, String dateSubmitted)
+    {
+        SQLMethods dbconn = new SQLMethods();
+        File filePath = null;
+        
+        
+        ResultSet result = dbconn.searchID("pendingjobs", firstName, lastName, fileName, dateSubmitted);
+                        
+        try 
+        {
+            
+            if (result.next())
+                filePath = new File(result.getString("filePath"));
+            
+            dbconn.closeDBConnection();
+            
+        } 
+        catch (SQLException ex) 
+        {
+            Logger.getLogger(UtilController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return filePath;
+        
+    }
+    
+     /**
+      * This is probably something that should be in a general Utils class for the front end or the various "views".
+      * I'm leaving this here for now because I don't want to change or add anything else that could affect other 
+      * groups. -Nick
+      */
+    public static int getSelectedRowNum(DefaultTableModel dm, int selectedRow, int column)
+    {
+        if (selectedRow < 0)
+            return -1;
+        
+        for (int i = 0; i < dm.getRowCount(); i++)
+            if (dm.getValueAt(i, column).equals(dm.getValueAt(selectedRow, column)))
+                return i;
+        
+        return -1;
+    }
+    
+    /* This function takes the column names found in the queryResult and inserts them into columnNames */
+    private static ArrayList<String> getColumnNames(ResultSet queryResult)
+    { 
+        ArrayList<String> columnNames = new ArrayList<>();
+        
+        try 
+        {
+            /* Meta data contains number of columns and there names.
+             * This is used to get entire rows of data in order to put into dataVector
+             * -Nick
+             */
+            ResultSetMetaData meta = queryResult.getMetaData();
+            int columnCount = meta.getColumnCount();
+            
+            for(int column = 1; column <= columnCount; column++)
+                columnNames.add(meta.getColumnName(column));
+        } 
+        catch (SQLException ex) 
+        {
+            Logger.getLogger(SQLMethods.class.getName()).log(Level.SEVERE, null, ex);
+            return columnNames;
+        }
+        
+        return columnNames;
+        
+    }
+    
+     /* This function is called to take the query result "ResultSet" return type form a DB query and re-format it into 
+      * general structures that make it easy for the View classes to display / modify.
+      * Therefore the View classes would not have to know about the data and column names ect... 
+      * -Nick
+      */
+    private static ArrayList<ArrayList<String>> readyOutputForViewPage(ResultSet queryResult)
+    {
+        ArrayList<String> columnNames = getColumnNames(queryResult);
+        ArrayList<ArrayList<String>> retval = new ArrayList<>();
+        
+        /* Process data column by column and add that data into dataVector */
+        try 
+        {  
+            ArrayList<String> tempRow;
+            
+            /* Goes through ResultSet row by row adding the row data into retval as an arraylist of type string */
+            while (queryResult.next()) 
+            {
+                tempRow = new ArrayList<>();
+                for (String columnName : columnNames) 
+                    tempRow.add(queryResult.getString((String) columnName));
+                
+                retval.add(tempRow);
+            }
+        }
+        catch (SQLException sqle)
+        {
+            System.out.println(sqle);
+            return retval;
+        }
+        return retval;
+    }
+    
+    /* I'm still considering making the return type the array of arrylists<String> since it is more general
+     * than having the UI pass in its table model -Nick
+    */
+    public static void updatePendingTableData(DefaultTableModel dataHolder)
+    {     
+        SQLMethods dbconn = new SQLMethods();
+        ResultSet queryResult = dbconn.searchPending();
+        
+        ArrayList<ArrayList<String>> retval = readyOutputForViewPage(queryResult);
+        
+        /* Must process results found in ResultSet before the connection is closed! */
+        dbconn.closeDBConnection();
+
+        for (ArrayList<String> retval1 : retval) 
+            dataHolder.addRow(retval1.toArray());
+    }
+     
+    
+}
