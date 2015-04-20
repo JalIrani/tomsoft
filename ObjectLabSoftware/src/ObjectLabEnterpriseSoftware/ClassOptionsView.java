837f405dacedb11ac8d0b79fa06bebf9cbe7b42a
kmoo <kmoorm2@students.towson.edu>
2015-04-20 16:26:00 -0400

    Removed class header

--- ObjectLabSoftware/src/ObjectLabEnterpriseSoftware/ClassOptionsView.java
+++ ObjectLabSoftware/src/ObjectLabEnterpriseSoftware/ClassOptionsView.java
@@ -8,15 +8,6 @@ import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Matt
- */
 public class ClassOptionsView extends javax.swing.JFrame {
 
     /**
