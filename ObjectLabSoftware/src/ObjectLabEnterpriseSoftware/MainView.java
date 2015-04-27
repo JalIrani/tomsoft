83ee79361506591c035b6554b3be51e8c04fc475
kmoo <kmoorm2@students.towson.edu>
2015-04-27 10:32:48 -0400

    a

--- ObjectLabSoftware/src/ObjectLabEnterpriseSoftware/MainView.java
+++ ObjectLabSoftware/src/ObjectLabEnterpriseSoftware/MainView.java
@@ -1,21 +1,10 @@
 package ObjectLabEnterpriseSoftware;
 
-import java.awt.Desktop;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 public class MainView extends javax.swing.JFrame
 {
     private static final String NAME_OF_PAGE = "Home";
